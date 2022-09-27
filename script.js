f528671dfee04e60da526d26e1444e61ed7b78e4
Marcus-Silva <92951909+SilvaMarcus@users.noreply.github.com>
2022-09-26 22:16:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
+
 var botao = document.querySelector('.botao');
 var caixa_img = document.querySelector('#QRcodeImage');
-const main_qr = document.querySelector('#main-qr');
- 
+
+
 botao.addEventListener('click', clicar => {
-
+  var usuario = document.querySelector('.usuario').value; 
+  var senha = document.querySelector('.senha').value;
+      
 // Pegando URL da API do GoogleCharts e concatenando usuario e senha com a url da API  
-var googleChartAPI = `https://chart.googleapis.com/chart?cht=qr&chl=WIFI%3AT%3AWPA%3BS%3A${usuario}%3BP%3A${senha}%3BH%3A%3B&chs=280x280&choe=UTF-8&chld=L|2`; 
+var googleChartAPI = `https://chart.googleapis.com/chart?cht=qr&chl=WIFI%3AT%3AWPA%3BS%3A${usuario}%3BP%3A${senha}%3BH%3A%3B&chs=250x250&choe=UTF-8&chld=L|2`; 
 
-var usuario = document.querySelector('.usuario').value; 
-var senha = document.querySelector('.senha').value;
-                                                                       
+                                                                   
 caixa_img.src = googleChartAPI; // aadicionando o link  no src de img 
 
-exibir() // chamando a funcao que exibe a div de imprimir 
+exibir() 
+
  })
-  
- const botao_print = document.querySelector('.fa-print');
 
-//funcao que adiciona classe para exibir botao de imprimir apos gerar o qr code
+
+
+// funcao adionando classes para ocultar e exibir 
+const botao_print = document.querySelector('.fa-print');
+const  input1 = document.querySelector('.input-usuario');
+const  input2 = document.querySelector('.input-senha');
+const titulo = document.querySelector('#titulo-qr');
 function exibir () {
-
-    botao_print.classList.toggle('show');
+  botao_print.classList.toggle('show');
+  input1.classList.toggle('hide');
+  input2.classList.toggle('hide');
+  titulo.classList.toggle('hide');
 
 }
 
+ // printar 
 botao_print.addEventListener('click', printar => {
-
+  
     print()
 
 })
